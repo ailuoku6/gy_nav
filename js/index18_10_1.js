9bfe7f262b71959b7e71e5e7a8961f3ec30af0a9
ailuoku6 <ailuoku6@ailuoku6deMBP.lan>
2018-10-01 21:56:52 +0800

    add github search

--- js/index18_10_1.js
+++ js/index18_10_1.js
@@ -113,6 +113,7 @@ var allsite = new Vue({
       {Marchine_name:"搜狗",button_value:"搜狗搜索",searApi:"https://www.sogou.com/web?query=",searApi_weizui:"",color:"#ff5943"},
       {Marchine_name:"360搜索",button_value:"360搜",searApi:"https://www.so.com/s?ie=utf-8&fr=none&src=360sou_newhome&q=",searApi_weizui:"",color:"#19b955"},
       {Marchine_name:"必应搜索",button_value:"必应搜索",searApi:"https://www.bing.com/search?q=",searApi_weizui:"",color:"#1688b1"},
+      {Marchine_name:"Github",button_value:"Github",searApi:"https://github.com/search?q=",searApi_weizui:"",color:"#00302e"},
       {Marchine_name:"知乎搜索",button_value:"知乎搜索",searApi:"https://www.zhihu.com/search?type=content&q=",searApi_weizui:"",color:"#0077e6"},
       {Marchine_name:"百度文库",button_value:"搜文库",searApi:"https://wk.baidu.com/search?word=",searApi_weizui:"",color:"#38F"},
       {Marchine_name:"图片搜索",button_value:"图片搜索",searApi:"http://image.so.com/i?q=",searApi_weizui:"&src=srp",color:"#19b955"},
