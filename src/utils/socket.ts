16c9035a80f3ab0bc73ca29e7dfbb41d13cdc5b4
ailuoku6 <ailuoku6@qq.com>
2022-07-10 18:32:03 +0800

    feat: socket manager

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -2,11 +2,55 @@ import { io as IO, Socket } from "socket.io-client";
 
 const localUrl = "ws://127.0.0.1:3001";
 
+// 负责socket通信，并将事件暴露给react app
 class SocketManager {
   private io: Socket;
+
+  private eventHandlers: Map<string, ((...arg: any[]) => void)[]>;
+
   constructor() {
     this.io = IO(localUrl, { transports: ["websocket"] });
-    console.info("-------socket_connnect");
+    this.eventHandlers = new Map();
+    this.initEventListener();
+  }
+
+  private initEventListener() {
+    this.io.onAny((event: string, restArg: any[]) => {
+      const callbacks = this.eventHandlers.get(event) || [];
+      const data = restArg[0];
+      callbacks.forEach((cb) => {
+        cb(data);
+      });
+    });
+  }
+
+  public reconnect() {
+    if (!this.io.connected) {
+      this.io.connect();
+    }
+  }
+
+  public disconnect() {
+    this.io.disconnect();
+  }
+
+  public onMsg(event: string, callback: (...arg: any[]) => void) {
+    const callbacks = this.eventHandlers.get(event) || [];
+    callbacks.push(callback);
+    this.eventHandlers.set(event, callbacks);
+
+    const unsubscript = () => {
+      const callbacks = (this.eventHandlers.get(event) || []).filter(
+        (cb) => cb !== callback
+      );
+      this.eventHandlers.set(event, callbacks);
+    };
+
+    return unsubscript;
+  }
+
+  public sendMsg(event: string, data: any) {
+    this.io.emit(event, data);
   }
 }
 
