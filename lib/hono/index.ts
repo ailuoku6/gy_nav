b9c08e4e58960b8ecb7a6e1b847ddd09b1bf993c
ailuoku6 <ailuoku6@qq.com>
2024-06-19 00:30:53 +0800

    chore: save

--- lib/hono/index.ts
+++ lib/hono/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 
 type Bindings = {
-//   MY_KV: KVNamespace;
+  //   MY_KV: KVNamespace;
   DB: D1Database;
 };
 
@@ -9,14 +9,14 @@ const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("/api", (ctx) => ctx.text("Hello world, this is Hono!!"));
 app.get("/api/users", async (ctx) => {
-  console.info('---------',ctx.env.DB);
-  const ps = ctx.env.DB.prepare("SELECT * from users");
-  const data = await ps.all();  
   let res = null;
   try {
+    console.info("---------", ctx.env.DB);
+    const ps = ctx.env.DB.prepare("SELECT * from users");
+    const data = await ps.all();
     res = JSON.stringify(data.results);
   } catch (error) {
-    res = JSON.stringify(error)
+    res = JSON.stringify(error);
   }
   ctx.text(`Hello users!!,${res}`);
 });
