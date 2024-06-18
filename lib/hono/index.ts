ff68557d9793efdebf01c18581628b39951c03b3
ailuoku6 <ailuoku6@qq.com>
2024-06-18 22:19:01 +0800

    chore: save

--- lib/hono/index.ts
+++ lib/hono/index.ts
@@ -11,7 +11,8 @@ app.get("/api", (ctx) => ctx.text("Hello world, this is Hono!!"));
 app.get("/api/users", async (ctx) => {
   const ps = ctx.env.DB.prepare("SELECT * from users");
   const data = await ps.all();
-  ctx.text("Hello users!!",JSON.stringify(data.results));
+  
+  ctx.text("Hello users!!",JSON.stringify(data.results.map(item=>item as any)) as any);
 });
 
 export default app;
