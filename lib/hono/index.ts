f814a88ba9d4e01aba9a76e693fc9536f28cd258
ailuoku6 <ailuoku6@qq.com>
2024-06-18 21:04:00 +0800

    chore: save

--- lib/hono/index.ts
+++ lib/hono/index.ts
@@ -2,5 +2,6 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/api", (ctx) => ctx.text("Hello world, this is Hono!!"));
+app.get("/api/users", (ctx) => ctx.text("Hello users!!"));
 
 export default app;
