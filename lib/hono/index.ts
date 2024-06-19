56f3d0aed8cdb9aca9ef570df59563bd88e3bc87
ailuoku6 <ailuoku6@qq.com>
2024-06-19 19:04:53 +0800

    feat: jwt test

--- lib/hono/index.ts
+++ lib/hono/index.ts
@@ -1,23 +1,60 @@
 import { Hono } from "hono";
 
-import { jwt } from "hono/jwt";
+import { jwt, sign as sign1, decode,verify } from "hono/jwt";
 
 import { Bindings } from "./types";
+import errorHandle from "./middleware/errorHandle";
 
 const app = new Hono<{ Bindings: Bindings }>();
 
+const signToen = (data: any, key: string) => {
+  const time = Math.floor(new Date().getTime() / 1000);
+  const expTime = time + 30 * 24 * 60 * 60;
+  return sign1({ ...data, exp: expTime, iat: time },key);
+};
+
+const authFreeSet = new Set([
+  "/api/auth_free",
+  "/api/login",
+  "/api/signup",
+  "/api/getAllFS",
+]);
+
 app.use("/api/*", (c, next) => {
+  if (authFreeSet.has(c.req.path)) {
+    return next();
+  }
+
   const jwtMiddleware = jwt({
-    secret: c.env.TokenSecret || "",
+    secret: c.env.TokenSecret || "GY",
   });
   return jwtMiddleware(c, next);
 });
 
+app.use(errorHandle);
+
 app.get("/api", (ctx) => ctx.text("Hello world, this is Hono!!"));
+app.get("/api/auth_free", (ctx) => ctx.text("Hello world, auth_free"));
+app.get("/api/login", async (ctx) => {
+
+  const tokenSecret = ctx.env.TokenSecret || "GY";
+
+  const res2 = await signToen(
+    { key: 1, user:{name:'abdc'} },
+    tokenSecret
+  );
+  console.info("-------decodeData22", await verify(res2,tokenSecret));
+  return ctx.text(`${tokenSecret}-----${res2}`);
+});
 app.get("/api/users", async (ctx) => {
   let res = null;
+
+  const jwtPayload = ctx.get("jwtPayload");
+  const jwtPayloadJson = ctx.json(jwtPayload);
+
+  console.info("-------jwtPayloadJson", jwtPayloadJson);
+
   try {
-    console.info("---------", ctx.env.DB);
     const ps = ctx.env.DB.prepare("SELECT * from users");
     const data = await ps.all();
     res = JSON.stringify(data.results);
