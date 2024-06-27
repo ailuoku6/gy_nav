b3d396046b4a8691d85c4397768d66b0d91e92f2
ailuoku6 <ailuoku6@qq.com>
2024-06-27 19:27:55 +0800

    chore: save

--- src/component/MarginHead/MarginHead.jsx
+++ src/component/MarginHead/MarginHead.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import './index.css';
 
-class MarginHead extends React.Component {
-  // eslint-disable-next-line no-useless-constructor
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return <div className={'autoMargin'}></div>;
-  }
-}
+const MarginHead = () => {
+  return <div className="autoMargin" />;
+};
 
 export default MarginHead;
