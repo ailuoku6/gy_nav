989388c5f2da8e3b386e95bdb747db30607eee1d
ailuoku6 <ailuoku6@qq.com>
2024-06-30 17:50:12 +0800

    chore: save

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-function NotFound(props) {
+function NotFound() {
   return (
     <div>
       <h1>404 Not Found</h1>
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,9 @@
 //引入react jsx写法的必须
-import React from 'react';
+// import React from 'react';
 //引入需要用到的页面组件
+// @ts-ignore
 import Home from '../pages/Home';
+// @ts-ignore
 import Login from '../pages/Login';
 import NotFound from '../pages/NotFound';
 // import About from './pages/about';
@@ -30,17 +32,6 @@ const route = (
 
 function router() {
   return route;
-  // return (
-  //     <Router>
-  //         <Switch>
-  //             {/*<Redirect from="/" exact to="/index"/>*/}
-  //             <Route exact path="/login" component={Login}/>
-  //             <Route exact path="/" component={Home} />
-
-  //         </Switch>
-
-  //         {/*<Route path="/about" component={About} />*/}
-  //     </Router>);
 }
 export { history };
 export default router;
