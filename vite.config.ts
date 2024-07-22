828df506c47351c0aa9217092445251681e266aa
ailuoku6 <ailuoku6@qq.com>
2024-07-22 20:30:31 +0800

    feat: drop log

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
   plugins: [
     react(),
     visualizer({
