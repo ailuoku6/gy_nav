a22d653e44a643323b9b3bab012e3a5e3e89f908
ailuoku6 <ailuoku6@qq.com>
2026-02-07 19:43:36 +0800

    --story=0 save

--- lib/hono/SQL/popularSites.sql
+++ lib/hono/SQL/popularSites.sql
@@ -1,7 +1,6 @@
-CREATE TABLE IF NOT EXISTS popularSites (
+CREATE TABLE popularSites (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     userId INTEGER NOT NULL UNIQUE,
     popularSites TEXT,
-    modifyDate DATETIME,
-    FOREIGN KEY (userId) REFERENCES users(id)
+    modifyDate DATETIME
 );
